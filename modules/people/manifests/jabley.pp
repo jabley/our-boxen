bd505a79e30aa91a0e65fbb0de7633c00854034e
James Abley <james.abley@gmail.com>
2015-12-14 09:09:38 +0000

    [jabley] fix capslock remapping
    See https://github.com/boxen/puppet-osx/issues/94
    
    This configures it correctly on Yosemite at least.

--- modules/people/manifests/jabley.pp
+++ modules/people/manifests/jabley.pp
@@ -103,6 +103,18 @@ class people::jabley(
     }
   osx::recovery_message { 'If found, please call +447796 692 786': }
 
+  class osx::keyboard::capslock_to_control {
+    # Remap capslock to control on all attached keyboards
+    $keyboard_ids = 'ioreg -n IOHIDKeyboard -r | grep -E \'VendorID"|ProductID\' | awk \'{ print $4 }\' | paste -s -d\'-\n\' -'
+    $check = 'xargs -I{} sh -c \'defaults -currentHost read -g "com.apple.keyboard.modifiermapping.{}-0" | grep "Dst = 2" > /dev/null\''
+    $remap = 'xargs -I{} defaults -currentHost write -g "com.apple.keyboard.modifiermapping.{}-0" -array "<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>" "<dict><key>HIDKeyboardModifierMappingDst</key><integer>0</integer><key>HIDKeyboardModifierMappingSrc</key><integer>2</integer></dict>" "<dict><key>HIDKeyboardModifierMappingDst</key><integer>0</integer><key>HIDKeyboardModifierMappingSrc</key><integer>10</integer></dict>"'
+    exec { 'Remap capslock to control on all keyboards':
+      command => "${keyboard_ids} | ${remap}",
+      unless  => "${keyboard_ids} | ${check}"
+    }
+  }
+
+
 #  sysctl::set { 'kern.maxfiles':
 #    value => '16384'
 #  }
